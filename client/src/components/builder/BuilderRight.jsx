3fc70747395447e811da483107e777dcc4aa3e9e
daoistcoder <deservekindness@gmail.com>
2023-07-18 23:46:48 +0800

    create builderRight

---
+++ client/src/components/builder/BuilderRight.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BuilderRight() {
+  return (
+    <div style={{ backgroundColor: "red", width: '50%' }}>
+      BuilderRight
+    </div >
+  )
+}
