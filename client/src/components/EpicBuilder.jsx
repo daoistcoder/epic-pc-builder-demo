9ab53d7f9208e842d8c8f012405d04fce11f3a20
daoistcoder <deservekindness@gmail.com>
2023-07-18 23:46:17 +0800

    making the divs of builder

--- client/src/components/EpicBuilder.jsx
+++ client/src/components/EpicBuilder.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import BuilderLeft from './builder/BuilderLeft'
+import BuilderRight from './builder/BuilderRight'
 
 export default function EpicBuilder() {
   return (
-    <div className='builder'>
-      EpicBuilder
+    <div className='builder' style={{display: 'flex'}}>
+      <BuilderLeft />
+      <BuilderRight />
     </div>
   )
 }
