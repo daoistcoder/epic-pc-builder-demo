e265cf8ea703a9b25c10c2b36e2ac22caeef3f99
daoistcoder <deservekindness@gmail.com>
2023-07-18 23:53:18 +0800

    adjust builder width

--- client/src/components/builder/BuilderLeft.jsx
+++ client/src/components/builder/BuilderLeft.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BuilderLeft() {
   return (
-    <div style={{ backgroundColor: 'blue', width: '50%' }}>
+    <div style={{ backgroundColor: 'blue', width: '65%' }}>
       BuilderLeft
     </div>
   )
--- client/src/components/builder/BuilderRight.jsx
+++ client/src/components/builder/BuilderRight.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BuilderRight() {
   return (
-    <div style={{ backgroundColor: "red", width: '50%' }}>
+    <div style={{ backgroundColor: "red", width: '35%' }}>
       BuilderRight
     </div >
   )
