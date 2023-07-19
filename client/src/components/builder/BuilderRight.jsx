6a7ea091d1fe9b54ac0d054ec5e98df25fc44077
daoistcoder <deservekindness@gmail.com>
2023-07-20 00:23:20 +0800

    create invoiceComp and update css

--- client/src/components/builder/BuilderRight.jsx
+++ client/src/components/builder/BuilderRight.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import InvoiceComponent from './builder_right/InvoiceComponent'
 
 export default function BuilderRight() {
   return (
-    <div style={{ backgroundColor: 'rgb(39, 39, 37)', width: '35%' }}>
-      BuilderRight
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(39, 39, 37)',
+        width: '35%'
+      }}>
+      <InvoiceComponent />
     </div >
   )
 }
---
+++ client/src/components/builder/builder_right/InvoiceComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function InvoiceComponent() {
+  return (
+    <div
+      style={{
+        width: '80%',
+        height: '95%',
+        backgroundColor: 'rgb(29, 29, 27)',
+      }}
+    >
+      InvoiceComponent
+    </div>
+  )
+}
