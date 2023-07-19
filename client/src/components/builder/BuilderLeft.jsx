6568394bb03ef9b788283207dd166176cc8ce93d
daoistcoder <deservekindness@gmail.com>
2023-07-20 00:01:55 +0800

    update builder comp

--- client/src/components/builder/BuilderLeft.jsx
+++ client/src/components/builder/BuilderLeft.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Prebuilt from './builder_left/Prebuilt'
+import Custombuild from './builder_left/Custombuild'
 
 export default function BuilderLeft() {
   return (
-    <div style={{ backgroundColor: 'rgb(29, 29, 27)', width: '65%' }}>
-      BuilderLeft
+    <div 
+      style={{ 
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        backgroundColor: 'rgb(29, 29, 27)',
+        width: '65%',
+         }}>
+      <Prebuilt />
+      <Custombuild />
     </div>
   )
 }
